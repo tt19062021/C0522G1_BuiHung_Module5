2cbad2c1cfc36787d7df4354f6b59dca385a442a
C0522G1-buihung <tt19062021@gmail.com>
2022-10-17 20:07:58 +0700

    up

--- 02_typescript/exercise/typescript-fibonaci/src/main.js
+++ 02_typescript/exercise/typescript-fibonaci/src/main.js
@@ -1,14 +1,16 @@
-var fibo1 = 0;
-var fibo2 = 1;
-var fibo3;
-var sum = 0;
-var fibos = [];
-for (var i = 1; i < 10; i++) {
-    fibo3 = fibo1 + fibo2;
-    fibos.push(fibo3);
-    sum += fibo3;
-    fibo1 = fibo2;
-    fibo2 = fibo3;
+function sumFibonaci(listFibo) {
+    var fibo1 = 0;
+    var fibo2 = 1;
+    var fibo3;
+    var sum = fibo1 + fibo2;
+    var fibos = [0, 1];
+    for (var i = 2; i < listFibo; i++) {
+        fibo3 = fibo1 + fibo2;
+        fibos.push(fibo3);
+        sum += fibo3;
+        fibo1 = fibo2;
+        fibo2 = fibo3;
+    }
+    return "Dãy fibonacci : [" + fibos + "] Tổng số: " + sum;
 }
-console.log("Sum : ".concat(sum));
-console.log("D\u00E3y Fibonacci : ".concat(fibos));
+console.log(sumFibonaci(5));
--- 02_typescript/exercise/typescript-fibonaci/src/main.ts
+++ 02_typescript/exercise/typescript-fibonaci/src/main.ts
@@ -1,16 +1,19 @@
-let fibo1: number = 0;
-let fibo2: number =1;
-let fibo3: number;
-let sum: number = 0;
-let fibos: Array<number> =[];
+function sumFibonaci(listFibo: number): string {
+    let fibo1: number = 0;
+    let fibo2: number = 1;
+    let fibo3: number;
+    let sum: number = fibo1 + fibo2;
+    let fibos: Array<number> = [0,1];
 
-for(let i =1 ; i < 10; i++){
-    fibo3 = fibo1 + fibo2;
-    fibos.push(fibo3);
-    sum += fibo3;
-    fibo1 = fibo2;
-    fibo2 = fibo3;
+    for (let i = 2; i < listFibo; i++) {
+        fibo3 = fibo1 + fibo2;
+        fibos.push(fibo3);
+        sum += fibo3;
+        fibo1 = fibo2;
+        fibo2 = fibo3;
+    }
+    return "Dãy fibonacci : ["+ fibos + "] Tổng số: " + sum;
 }
 
-console.log(`Sum : ${sum}`);
-console.log(`Dãy Fibonacci : ${fibos}`);
+console.log(sumFibonaci(5));
+
